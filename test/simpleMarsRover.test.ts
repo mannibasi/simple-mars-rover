3b20f4eb903a2d67f5529edd94084e0af78b6256
= <manni.basi@gmail.com>
2026-01-10 16:38:13 +0000

    Added more tests that felt appropriate

--- test/simpleMarsRover.test.ts
+++ test/simpleMarsRover.test.ts
@@ -6,18 +6,23 @@ describe("Simple Mars Rover", () => {
     { command: "LL", expected: "0:0:S" },
     { command: "LLL", expected: "0:0:E" },
     { command: "LLLL", expected: "0:0:N" },
+    { command: "LLLLL", expected: "0:0:W" },
     { command: "R", expected: "0:0:E" },
     { command: "RR", expected: "0:0:S" },
     { command: "RRR", expected: "0:0:W" },
     { command: "RRRR", expected: "0:0:N" },
+    { command: "RRRRR", expected: "0:0:E" },
     { command: "M", expected: "0:1:N" },
     { command: "RM", expected: "1:0:E" },
     { command: "MM", expected: "0:2:N" },
+    { command: "MMMRM", expected: "1:3:E" },
+    { command: "MMRMRM", expected: "1:1:S" },
     { command: "MMMMMMMMMM", expected: "0:0:N" },
     { command: "RMMMMMMMMM", expected: "9:0:E" },
     { command: "RMMMMMMMMMM", expected: "0:0:E" },
     { command: "LM", expected: "9:0:W" },
-    { command: "RRM", expected: "0:9:S" }
+    { command: "RRM", expected: "0:9:S" },
+    { command: "LLM", expected: "0:9:S" }
   ])(
     "Command '$command' results in position '$expected'",
     ({ command, expected }) => {
