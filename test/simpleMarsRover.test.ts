80899ce69e708ce4c4264f77100ae14be6502d59
= <manni.basi@gmail.com>
2026-01-10 14:10:58 +0000

    Parameterized tests to remove duplication

--- test/simpleMarsRover.test.ts
+++ test/simpleMarsRover.test.ts
@@ -1,24 +1,17 @@
 import { MarsRover } from "../src/marsRover";
 
 describe("Simple Mars Rover", () => {
-  test("Facing 'West' after turn 'Left' command", () => {
-    const rover = new MarsRover();
-    expect(rover.command("L")).toBe("0:0:W");
-  });
-  test("Facing 'East' after turn 'Right' command", () => {
-    const rover = new MarsRover();
-    expect(rover.command("R")).toBe("0:0:E");
-  });
-  test("Facing 'East' after turn 'Right' and 'Move' command", () => {
-    const rover = new MarsRover();
-    expect(rover.command("RM")).toBe("1:0:E");
-  });
-  test("Facing 'North' after 'Move' command", () => {
-    const rover = new MarsRover();
-    expect(rover.command("M")).toBe("0:1:N");
-  });
-  test("Facing 'North' (with a vertical position of 2) after two 'Move' commands", () => {
-    const rover = new MarsRover();
-    expect(rover.command("MM")).toBe("0:2:N");
-  });
+  test.each([
+    { command: "L", expected: "0:0:W" },
+    { command: "R", expected: "0:0:E" },
+    { command: "M", expected: "0:1:N" },
+    { command: "RM", expected: "1:0:E" },
+    { command: "MM", expected: "0:2:N" }
+  ])(
+    "Command '$command' results in position '$expected'",
+    ({ command, expected }) => {
+      const rover = new MarsRover();
+      expect(rover.command(command)).toBe(expected);
+    }
+  );
 });
