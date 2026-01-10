d7ae72eecb958afb036db65e8381385fcbf02b58
= <manni.basi@gmail.com>
2026-01-10 13:44:21 +0000

    Added failing test for Right + Move command

--- test/simpleMarsRover.test.ts
+++ test/simpleMarsRover.test.ts
@@ -9,6 +9,10 @@ describe("Simple Mars Rover", () => {
     const rover = new MarsRover();
     expect(rover.command("R")).toBe("0:0:E");
   });
+  test("Facing 'East' after turn 'Right' and 'Move' command", () => {
+    const rover = new MarsRover();
+    expect(rover.command("RM")).toBe("1:0:E");
+  });
   test("Facing 'North' after 'Move' command", () => {
     const rover = new MarsRover();
     expect(rover.command("M")).toBe("0:1:N");
